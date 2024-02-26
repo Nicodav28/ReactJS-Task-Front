73c90c7eed8927235c78f5733907125c3a3e03b1
Nicolas David Estevez <nicolas.estevez@infodeclat.com>
2024-02-25 21:12:22 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+
+
+function App() {
+  return (
+    <h1 className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+      <div className="bg-gray-950 p-4 w-2/5">
+        <h1 className="text-3xl font-bold text-center block my-2">Task App</h1>
+        <TaskList/>
+        <TaskForm/>
+      </div>
+    </h1>
+  )
+}
+
+export default App
